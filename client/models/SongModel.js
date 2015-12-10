04515e0f2029bd607638916c3f7e976165215ac8
jpanopio <jcpanopio1@gmail.com>
2015-12-09 16:35:17 -0800

    Implement enqueueing

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,16 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  // queued: false,
+
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
-  }
+  },
 
+  enqueue: function(){
+    console.log("Enqueue");
+    // queued: true;
+    this.trigger('enqueue', this);
+  }
 });
